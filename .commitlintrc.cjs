c7d156f9b04457e36d89cba913f5978b4964d65c
Jace Babin <jbabin91@gmail.com>
2024-04-04 19:18:25 -0700

    feat: :sparkles: init project
    chore: :hammer: update package type

---
+++ .commitlintrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("cz-git").UserConfig} */
+module.exports = {
+  alias: {
+    b: "chore: :hammer: bump dependencies",
+  },
+  maxSubjectLength: 100,
+  allowCustomIssuePrefix: false,
+  allowEmptyIssuePrefix: false,
+  prompt: {
+    useEmoji: true,
+  },
+  skipQuestions: ["footer", "scope"],
+};

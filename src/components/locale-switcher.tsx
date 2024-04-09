b8b3557075d3a3ff75d77f30a1add017121ad775
Jace Babin <jbabin91@gmail.com>
2024-04-09 16:02:16 -0700

    feat: :sparkles: add locale switcher

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -1,3 +1,46 @@
+'use client';
+
+import { useLocale } from 'next-intl';
+
+import { Button } from '@/components/ui/Button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { siteConfig } from '@/configs/siteConfig';
+import { usePathname, useRouter } from '@/libs/i18nNavigation';
+
+import { Icons } from './icons';
+
 export function LocaleSwitcher() {
-  return <div>Locale</div>;
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
+
+  const handleChange = (value: string) => {
+    router.push(pathname, { locale: value });
+    router.refresh();
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button size="icon" variant="secondary">
+          <Icons.Globe />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuRadioGroup value={locale} onValueChange={handleChange}>
+          {siteConfig.locales.map((locale) => (
+            <DropdownMenuRadioItem key={locale.id} value={locale.id}>
+              {locale.name}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
